939139c5aff208e3b9d49d2d28a1a1bcfb4f4dec
Rudolf <rudoxvanx@gmail.com>
2020-09-04 13:10:56 -0500

    Ejemplo let/const

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,19 @@
+//Variable y Constantes 
+
+/*Constantes: Una vez definido su valor este ya no puede
+ ser sobreescrito*/
+
+ const nombre ='Rodolfo';
+ let apellido = 'Garcia'
+
+ let valorDado = 5;
+ valorDado = 4;
+ console.log(nombre,apellido)
+
+ //var ya quedo en deshuso
+ if(true){
+     let valorDado = 6;
+     console.log(valorDado);
+ }
+
+ console.log(valorDado);
