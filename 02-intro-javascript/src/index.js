c9ac10741a48f9553dbc6e8962a6a036f3d82423
Rudolf <rudoxvanx@gmail.com>
2020-09-05 23:23:41 -0500

    Ejemplo promesas

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,21 +1,5 @@
-const apiKey = 'eQEw5fsdvV37XYA3Wbd512lb2JQ66KzS';
-
-const peticion = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
-
-/*
-peticion.then(resp => {
-    //console.log(resp);
-    resp.json().then(data => {
-        console.log(data);
-    })
-})*/
-peticion
-    .then(resp => resp.json())
-    .then(({data}) => {
-        const {url} = data.images.original;
-        console.log(url);
-        const img = document.createElement('img');
-        img.src= url;
-        document.body.append(img);
-    })
-    .catch(console.warn);
+const getImagenPromesa = () => {
+    const promesa = new Promise((resolve,reject) =>{
+        resolve()
+    });
+}
