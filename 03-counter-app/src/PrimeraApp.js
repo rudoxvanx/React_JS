ae2278cd67f34f240ff7e64bc55c86a92c795c2a
Rudolf <rudoxvanx@gmail.com>
2020-09-06 17:30:02 -0500

    Creación del primer componente

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+//irm + tab --> Nos importa de una manera más rápida
+
+/*Existen componentes del tipo clase y del tipo
+funcion.
+
+A los del tipo funcion se les llama funtional components
+estos ya admiten estados*/
+
+const PrimeraApp = () => {
+    return <h1>Hola mundo</h1>;
+}
+
+export default PrimeraApp;
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PrimeraApp from './PrimeraApp';
+
+import './index.css';
 
 const saludo = <h1>Hola mundo</h1>;
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(saludo,divRoot);
+ReactDOM.render(<PrimeraApp/> , divRoot);
 
 /*Usamos REACTDOM para renderizar ya que este nos permite
 crear nuestro arbol de componentes y comunicarnos entre ellos
