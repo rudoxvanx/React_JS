4ff8d4d3a8c398c3948fac7b3d4b271edeea0d57
Rudolf <rudoxvanx@gmail.com>
2020-09-07 17:30:14 -0500

    Implementación de evento click

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -4,10 +4,18 @@ import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
 
+    //handleAdd
+    const handleAdd =(e)=>{
+        console.log(e);
+    }
+
     return(
         <>
             <h1>CounterApp</h1>
             <h2> { value } </h2>
+            <button onClick={handleAdd}>+1</button>
+            {/*<button onClick={(e) => {handleAdd(e)}} >+1</button>*/}
+
         </>
     )
 }
