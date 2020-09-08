b7d0cdd455ddf95939e9ac945a923c5fdd8e65f9
Rudolf <rudoxvanx@gmail.com>
2020-09-07 19:12:34 -0500

    Implementación del hook useState

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
 
+    //const [ nombre,setNombre ] = useState('Goku');
+    //console.log(nombre,setNombre);
+    //const state = useState();
+    //console.log(state);
+    //console.log(state[1]());
+  
+
+    const [ counter,setCounter ] = useState(0); // []
+    /*USO DE HOOKS
+        Los hooks son funciones.
+
+        useState es un hook que regresa un arreglo de 
+    */
+
+
+    /*SETCOUNTER cambia el estado del counter por consecuencia se
+      dispara de nuevo la renderización del elemento. Solo se 
+      renderiza el elemento cambiante
+    */
+    
     //handleAdd
-    const handleAdd =(e)=>{
-        console.log(e);
+    const handleAdd = () => {
+        //No podemos poner counter++ ya que esto mutaria y no funcionaria
+        //setCounter(counter + 1);
+        setCounter((c) => c + 1);
+
     }
 
     return(
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
             <button onClick={handleAdd}>+1</button>
             {/*<button onClick={(e) => {handleAdd(e)}} >+1</button>*/}
 
