7d9f6d822ecd78c5df80740fd2162b76ad7fcc4b
Rudolf <rudoxvanx@gmail.com>
2020-09-10 18:10:37 -0500

    Prueba unitaria sobre 11-async-await

--- 03-counter-app/src/base/11-async-await.js
+++ 03-counter-app/src/base/11-async-await.js
@@ -3,23 +3,26 @@
 // const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
 // getImagenPromesa().then( console.log );
 
-const getImagen = async() => {
+export const getImagen = async() => {
 
     try {
 
-        const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
+        const apiKey = 'CI8c9AjX5BFgd2vOhX1Ovx5V2T6Thl15abcdefghi';
         const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
         const { data } = await resp.json(); 
 
         const { url } = data.images.original;
 
-        const img = document.createElement('img');
+        /*const img = document.createElement('img');
         img.src = url;
-        document.body.append( img );
+        document.body.append( img );*/
+
+        return url;
 
     } catch (error) {
         // manejo del error
-        console.error(error)
+        //console.error(error);
+        return 'No existe';
     }
     
     
