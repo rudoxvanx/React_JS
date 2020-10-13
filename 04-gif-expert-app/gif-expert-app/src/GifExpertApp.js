a86a5234e5c88b8baac6a912c00fab1e92264d2d
Rudolf <rudoxvanx@gmail.com>
2020-10-13 15:04:27 -0500

    creacion del proyecto app giphy

---
+++ 04-gif-expert-app/gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,14 @@
+import React, {} from 'react';
+
+
+/*Ordenar la estructura de archivos segun la necesidad de la aplicación*/
+const GifExpertApp = () => {
+    return (
+        <div>
+            <h2>GifExpertApp</h2>
+            <hr/>
+        </div>
+    )
+}
+
+export default GifExpertApp;
