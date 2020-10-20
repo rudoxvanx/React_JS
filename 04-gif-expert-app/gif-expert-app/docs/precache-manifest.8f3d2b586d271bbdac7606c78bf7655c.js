bdabd7ff1c8bd79db5c05b986e31ceead4ad87ee
Rudolf Garcia <rudoxvanx@gmail.com>
2020-10-20 15:49:35 -0500

    docs added

---
+++ 04-gif-expert-app/gif-expert-app/docs/precache-manifest.8f3d2b586d271bbdac7606c78bf7655c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c808f02e2829cf7b860f4df27d831c41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b82388a5ca3d60bc16e0",
+    "url": "/static/css/main.61838a94.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b82388a5ca3d60bc16e0",
+    "url": "/static/js/main.e025cded.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
